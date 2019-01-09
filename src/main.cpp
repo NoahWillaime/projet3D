7f42a804ba3e84f1e00a740986a5f7fb336b287f
willaime3u <noah.willaime@gmail.com>
2019-01-09 12:35:25 +0100

    fill Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -74,7 +74,11 @@ vector<vector<float> > readfile(char *filename){
   return tab;
 }
 
-void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
+vector<vector<int> > drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
+  vector<int> Vx;
+  vector<int> Vy;
+  vector<vector<int> > tab;
+
     bool steep = false; 
     if (std::abs(x0-x1)<std::abs(y0-y1)) { 
         std::swap(x0, y0); 
@@ -93,15 +97,31 @@ void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color){
     for (int x=x0; x<=x1; x++) { 
         if (steep) { 
             image.set(y, x, color); 
+	    Vx.push_back(y);
+	    Vy.push_back(x);
         } else { 
-            image.set(x, y, color); 
+            image.set(x, y, color);
+	    Vx.push_back(x);
+	    Vy.push_back(y);
         } 
         error2 += derror2; 
         if (error2 > dx) { 
             y += (y1>y0?1:-1); 
             error2 -= dx*2; 
         } 
-    } 
+    }
+    tab.push_back(Vx);
+    tab.push_back(Vy);
+    return tab;
+}
+
+void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, TGAImage &image, TGAColor color){
+  vector<vector<int> > line = drawLine(x1, y1, x2, y2, image, color);
+  drawLine(x2, y2, x3, y3, image, color);
+  drawLine(x3, y3, x1, y1, image, color);
+  for (int i = 0; i < line[0].size(); i++){
+    drawLine(x3, y3, line[0][i], line[1][i], image, color);
+  }
 }
 
 int world2screen(float x) {
@@ -118,15 +138,16 @@ int main(int argc, char **argv){
   float x1, x2, x3;
   float y1, y2, y3;
   TGAImage image(size, size, TGAImage::RGB);
-  for (int i = 0; i < line.size(); i+=3){
-    for (int j=0; j<3; j++) {
-      int Ax = world2screen(tab[0][line[i+j]]);
-      int Ay = world2screen(tab[1][line[i+j]]);
-      int Bx = world2screen(tab[0][line[i+(j+1)%3]]);
-      int By = world2screen(tab[1][line[i+(j+1)%3]]);
-      drawLine(Ax, Ay, Bx, By, image, white);
-    }
-  }
+  // for (int i = 0; i < line.size(); i+=3){
+  //   for (int j=0; j<3; j++) {
+  //     int Ax = world2screen(tab[0][line[i+j]]);
+  //     int Ay = world2screen(tab[1][line[i+j]]);
+  //     int Bx = world2screen(tab[0][line[i+(j+1)%3]]);
+  //     int By = world2screen(tab[1][line[i+(j+1)%3]]);
+  //     drawLine(Ax, Ay, Bx, By, image, white);
+  //   }
+  // }
+  drawTriangle(10, 10, 10, 300, 270, 90, image, red);
   image.flip_vertically();
   image.write_tga_file("output.tga");
 }
